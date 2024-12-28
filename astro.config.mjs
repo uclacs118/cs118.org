e63e2a74949ff863618929a7d35b902668d753e9
Omar Elamri <ea_do@icloud.com>
2024-12-28 01:38:00 +0100

    Add anchor links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,31 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()]
+    integrations: [mdx()],
+    markdown: {
+        rehypePlugins: [
+            rehypeSlug,
+            [
+                rehypeAutolinkHeadings,
+                {
+                    behavior: 'prepend',
+                    content: {
+                        type: 'text',
+                        value: '#',
+                    },
+                    headingProperties: {
+                        className: ['anchor'],
+                    },
+                    properties: {
+                        className: ['anchor-link'],
+                    },
+                },
+            ],
+        ],
+    }
 });
