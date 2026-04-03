5c3ff6cc5f31ab37541856e58652d69a8eb7e391
Eric Wang <89937277+nouturnsign@users.noreply.github.com>
2026-04-03 01:06:35 -0700

    Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,33 @@
 // @ts-check
-import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()],
-	markdown: {
-		rehypePlugins: [
-			rehypeSlug,
-			[
-				rehypeAutolinkHeadings,
-				{
-					behavior: 'prepend',
-					content: {
-						type: 'text',
-						value: '#',
-					},
-					headingProperties: {
-						className: ['anchor'],
-					},
-					properties: {
-						className: ['anchor-link'],
-					},
-				},
-			],
-		],
-	}
+  integrations: [mdx()],
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          content: {
+            type: "text",
+            value: "#",
+          },
+          headingProperties: {
+            className: ["anchor"],
+          },
+          properties: {
+            className: ["anchor-link"],
+          },
+        },
+      ],
+    ],
+  },
 });
